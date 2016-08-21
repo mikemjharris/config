db4b6d77937578249333a817915e89a55ba86262
Mike Harris <mike.mj.harris@gmail.com>
2016-08-21 20:16:08 +0100

    update variable name

--- bin/latest-branches.sh
+++ bin/latest-branches.sh
@@ -4,7 +4,7 @@
 LATEST_BRANCHES=$(git for-each-ref --sort=-committerdate --count=15 --format="ref=%(refname) %(*subject) $"  refs/heads/)
 
 # Parse to an array - IFS sets the word boundary to be the $ sign we appended previously
-IFS=$'$' read -rd '' -a ARRAY_BRANCHES<<<"$T"
+IFS=$'$' read -rd '' -a ARRAY_BRANCHES<<<"$LATEST_BRANCHES"
 
 for i in ${!ARRAY_BRANCHES[@]}; do 
     echo $(($i + 1)) ${ARRAY_BRANCHES[$i]};
