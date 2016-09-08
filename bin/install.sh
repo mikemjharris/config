0ef33ed868bc0c5b9d098543d8b0785ce9009404
Mike Harris <mike.mj.harris@gmail.com>
2016-09-08 10:48:38 +0100

    update install script for updated congig and add comments

--- bin/install.sh
+++ bin/install.sh
@@ -1,3 +1,12 @@
 # inspired by https://gist.github.com/mislav/5706063
 
+echo "Creating vimrc and tmux conf files"
+curl -fsSL "https://raw.githubusercontent.com/mikemjharris/config/master/conf/.vimrc" -o  ~/.vimrc
+
+curl -fsSL "https://raw.githubusercontent.com/mikemjharris/config/master/conf/.tmux.conf" -o  ~/.tmux.conf
+
+# TODO - check if the script has been run.
+# TODO - check to see which shell - mabe do for .zshrc
+# TODO - even better would be to have aliases in a seperate file and source that.
+echo "Adding alias' to bashrc"
 curl -fsSL "https://raw.githubusercontent.com/mikemjharris/config/master/alias-command-line" >> ~/.bashrc
