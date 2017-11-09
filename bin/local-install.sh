bcc202f84eda7435d23e0630671c62b66ee0bf4f
mikemjharris <mike.mj.harris@gmail.com>
2017-11-09 23:49:14 +0000

    Update local install

--- bin/local-install.sh
+++ bin/local-install.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-echo 'symlinks .tmux.conf and .vimrc along with others to your directory so that updates are easy to setup'
+echo "install pulg for local plugins. run - :PlugInstall"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+echo "install zsh shell"
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+echo "symlinks .tmux.conf and .vimrc along with others to your directory so that updates are easy to setup"
 ln -s $(pwd)/../conf/.tmux.conf  ~/.tmux.conf
 ln -s $(pwd)/../conf/.vimrc ~/.vimrc
+ln -s $(pwd)/../conf/.bash_aliases  ~/.bash_aliases
+
+echo "Adding alias to zshrc"
+curl -fsSL "https://raw.githubusercontent.com/mikemjharris/config/master/conf/setup_bash_aliases" >> ~/.zshrc
 
