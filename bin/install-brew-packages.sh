09930096dfbdfbdf59d3c6e5a88b8960b14ec0fc
mikemjharris <mike.mj.harris@gmail.com>
2017-11-10 13:16:04 +0000

    setup rbenv

--- bin/install-brew-packages.sh
+++ bin/install-brew-packages.sh
@@ -2,5 +2,12 @@
 
 echo "Brew installing various dependencies"
 brew update
-brew install rmagick nginx --with-passenger passenger wget tree openssh redis docker openssl ffmpeg  imagemagick htop postgres tmux vim jq
+brew install nginx --with-passenger passenger wget tree openssh redis docker openssl ffmpeg  imagemagick htop postgres tmux vim jq rbenv
+
+
+# https://github.com/rbenv/rbenv#homebrew-on-macos
+# installing rbenv needs this added to specific rc file 
+echo 'eval "$(rbenv init -)"' >> ~/.zshrc
+
+source ~/.zshrc
 
