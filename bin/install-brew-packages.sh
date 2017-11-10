fffc887a67ee0f53db7b47f8c0bd9f24f1985f5c
mikemjharris <mike.mj.harris@gmail.com>
2017-11-10 13:40:03 +0000

    add tmux dependency

--- bin/install-brew-packages.sh
+++ bin/install-brew-packages.sh
@@ -7,6 +7,10 @@ echo "Brew installing various dependencies"
 brew update
 brew install nginx --with-passenger passenger wget tree openssh redis docker openssl ffmpeg  imagemagick htop postgres tmux vim jq rbenv
 
+# This is required for my tmux conf setup to allow things like pbcopy / paste to work with tmux
+# https://github.com/ChrisJohnsen/tmux-MacOSX-pasteboard
+brew install reattach-to-user-namespace
+ 
 
 # https://github.com/rbenv/rbenv#homebrew-on-macos
 # installing rbenv needs this added to specific rc file 
