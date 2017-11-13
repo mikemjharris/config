d32a1a144394e3f593d393dd80ebc8ef2eeb4577
mikemjharris <mike.mj.harris@gmail.com>
2017-11-13 10:11:25 +0000

    add vim templates support

--- bin/local-install.sh
+++ bin/local-install.sh
@@ -8,9 +8,12 @@ echo "install zsh shell"
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 echo "symlinks .tmux.conf and .vimrc along with others to your directory so that updates are easy to setup"
-ln -s $(pwd)/../conf/.tmux.conf  ~/.tmux.conf
-ln -s $(pwd)/../conf/.vimrc ~/.vimrc
-ln -s $(pwd)/../conf/.bash_aliases  ~/.bash_aliases
+ln -s $(pwd)/conf/.tmux.conf  ~/.tmux.conf
+ln -s $(pwd)/conf/.vimrc ~/.vimrc
+ln -s $(pwd)/conf/.bash_aliases  ~/.bash_aliases
+
+echo "Linking in vim templates"
+ln -s $(pwd)/conf/vim-templates ~/.vim/templates
 
 echo "Adding alias to zshrc"
 curl -fsSL "https://raw.githubusercontent.com/mikemjharris/config/master/conf/setup_bash_aliases" >> ~/.zshrc
