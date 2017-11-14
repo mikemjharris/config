fbdb468acd24ed2c75cdda9dd3b78cbc64d32174
mikemjharris <mike.mj.harris@gmail.com>
2017-11-14 22:21:38 +0000

    add docker to brew cask installs

--- bin/install-brew-packages.sh
+++ bin/install-brew-packages.sh
@@ -5,14 +5,14 @@ echo "Installing homebrew (mac package manager)"
 
 echo "Brew installing various dependencies"
 brew update
-brew install nginx --with-passenger passenger wget tree openssh redis docker openssl ffmpeg  imagemagick htop postgres tmux vim jq rbenv the_silver_searcher 
+brew install nginx --with-passenger passenger wget tree openssh redis docker openssl ffmpeg  imagemagick htop postgres tmux vim jq rbenv the_silver_searcher ack
 
 # This is required for my tmux conf setup to allow things like pbcopy / paste to work with tmux
 # https://github.com/ChrisJohnsen/tmux-MacOSX-pasteboard
 brew install reattach-to-user-namespace
 
 # use brew cask for install applications https://caskroom.github.io/ 
-brew cask install gimp slack
+brew cask install gimp slack docker
 
 # https://github.com/rbenv/rbenv#homebrew-on-macos
 # installing rbenv needs this added to specific rc file 
