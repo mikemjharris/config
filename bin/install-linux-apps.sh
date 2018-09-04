608d3d3b431a47110d1d8857e94cadf3211905c6
Mike Harris <hello@mikemjharris.com>
2018-09-04 13:28:24 +0100

    install linux apps

---
+++ bin/install-linux-apps.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+snap update
+snap install gimp
+snap install spotify
+snap install elasticsearch
+snap install foreman-installer
+snap install foreman
+snap install chrome
+snap install sequeler
+snap install vscode --classic
+snap install --edge sqlitebrowser
+
+
