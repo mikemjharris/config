5dac64d687ac467937ed32968776b11cc8e40f05
Mike Harris <hello@mikemjharris.com>
2021-02-01 14:40:06 +0000

    feat(setup mappings for new keyboard)

---
+++ conf/keyboard/setup-keyboard.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Got a new akko keyboard.  Wanted to remap some of the keys without remapping for all keybaords
+
+# get the id for the keyboard we're using
+KEYBOARD_ID=$(
+  xinput list | 
+    sed -n 's/.*Akko.*Keyboard.*id=\([0-9]*\).*/\1/p'
+)
+
+[ "$KEYBOARD_ID" ] || exit
+
+# updload the new layout display
+xkbcomp -i $KEYBOARD_ID new-layout.xkb $DISPLAY
+
