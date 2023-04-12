d7f435b30c975b0ac8f9c8db955f92e83cd56a07
Mike Harris <hello@mikemjharris.com>
2023-04-12 21:39:49 +0100

    feat(combine plugins)

--- conf/new-nvim-setup/lua/core/plugin_config/lsp_config.lua
+++ conf/new-nvim-setup/lua/core/plugin_config/lsp_config.lua
@@ -112,3 +112,44 @@ keymap("n", "<Leader>co", "<cmd>Lspsaga outgoing_calls<CR>")
 -- Floating terminal
 keymap({"n", "t"}, "<A-d>", "<cmd>Lspsaga term_toggle<CR>")
 
+-- lsp sage jump to and change doesn't seem to work.  Here is a workaround
+-- calling vim diagnostic next and then the code action
+function nextChange() 
+  vim.diagnostic.goto_prev()
+  vim.cmd [[Lspsaga code_action]]
+end
+
+-- LSP Config setup - when lspsaga struggles
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', nextChange)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next )
+keymap('n', '<space>q', vim.diagnostic.setloclist)
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gS', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gs', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'J', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<space>f', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+  end,
+})
