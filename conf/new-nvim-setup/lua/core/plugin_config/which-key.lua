0e1c431eb45e9edb7dfbba01e8b28bf54ad02c5c
Mike Harris <hello@mikemjharris.com>
2023-11-23 18:36:05 +0000

    feat(add chatgpt to neovim setup)

--- conf/new-nvim-setup/lua/core/plugin_config/which-key.lua
+++ conf/new-nvim-setup/lua/core/plugin_config/which-key.lua
@@ -21,5 +21,22 @@ wk.register({
     n = { "New File" },                                                              -- just a label. don't create any mapping
     x = { function() print("This is an example of what you can do") end, "Foobar" }, -- you can also pass functions!
     c = { "<cmd> :let @+=@% <cr>", 'Copy file name' }
-  }
+  },
+  c = {
+    name = "ChatGPT",
+    c = { "<cmd>ChatGPT<CR>", "ChatGPT" },
+    e = { "<cmd>ChatGPTEditWithInstruction<CR>", "Edit with instruction", mode = { "n", "v" } },
+    g = { "<cmd>ChatGPTRun grammar_correction<CR>", "Grammar Correction", mode = { "n", "v" } },
+    t = { "<cmd>ChatGPTRun translate<CR>", "Translate", mode = { "n", "v" } },
+    k = { "<cmd>ChatGPTRun keywords<CR>", "Keywords", mode = { "n", "v" } },
+    d = { "<cmd>ChatGPTRun docstring<CR>", "Docstring", mode = { "n", "v" } },
+    a = { "<cmd>ChatGPTRun add_tests<CR>", "Add Tests", mode = { "n", "v" } },
+    o = { "<cmd>ChatGPTRun optimize_code<CR>", "Optimize Code", mode = { "n", "v" } },
+    s = { "<cmd>ChatGPTRun summarize<CR>", "Summarize", mode = { "n", "v" } },
+    f = { "<cmd>ChatGPTRun fix_bugs<CR>", "Fix Bugs", mode = { "n", "v" } },
+    x = { "<cmd>ChatGPTRun explain_code<CR>", "Explain Code", mode = { "n", "v" } },
+    r = { "<cmd>ChatGPTRun roxygen_edit<CR>", "Roxygen Edit", mode = { "n", "v" } },
+    l = { "<cmd>ChatGPTRun code_readability_analysis<CR>", "Code Readability Analysis", mode = { "n", "v" } },
+  },
+
 }, { prefix = "<leader>" })
--- conf/new-nvim-setup/lua/core/plugins.lua
+++ conf/new-nvim-setup/lua/core/plugins.lua
@@ -1,11 +1,11 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
     lazypath,
   })
 end
@@ -31,26 +31,36 @@ local plugins = {
   'hrsh7th/cmp-nvim-lsp',
   'L3MON4D3/LuaSnip',
   'saadparwaiz1/cmp_luasnip',
-  "rafamadriz/friendly-snippets",
-  "github/copilot.vim",                -- copilot
-  "williamboman/mason.nvim",           -- manage lsps etc.
-  "neovim/nvim-lspconfig",             --list of lsp server configs
-  "williamboman/mason-lspconfig.nvim", --link the two above
-  "glepnir/lspsaga.nvim",              -- lsp ui
+  'rafamadriz/friendly-snippets',
+  'github/copilot.vim',                -- copilot
+  'williamboman/mason.nvim',           -- manage lsps etc.
+  'neovim/nvim-lspconfig',             --list of lsp server configs
+  'williamboman/mason-lspconfig.nvim', --link the two above
+  'glepnir/lspsaga.nvim',              -- lsp ui
   'rking/ag.vim',                      -- ag search
   'Chun-Yang/vim-action-ag',           -- ag search word you are on
   'pappasam/papercolor-theme-slim',    -- colorscheme
   'norcalli/nvim-colorizer.lua',       -- see colors in nvim
   {
-    "jose-elias-alvarez/null-ls.nvim", -- linting - eslint too slow though
-    requires = { "nvim-lua/plenary.nvim" },
+    'jose-elias-alvarez/null-ls.nvim', -- linting - eslint too slow though
+    requires = { 'nvim-lua/plenary.nvim' },
   },
   'folke/neodev.nvim',
   'folke/which-key.nvim',
   'pwntester/octo.nvim',
   'numToStr/Comment.nvim',
+  'MunifTanjim/nui.nvim',
+  {
+    'jackMort/ChatGPT.nvim',
+    requires = {
+      'MunifTanjim/nui.nvim',
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope.nvim'
+    }
+  },
+
 }
 
 local opts = {}
 
-require("lazy").setup(plugins, opts)
+require('lazy').setup(plugins, opts)
