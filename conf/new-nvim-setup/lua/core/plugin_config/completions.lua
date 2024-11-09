cc9cce3cefe857dd731b185d5796e7b5fdb8b334
Mike Harris <hello@mikemjharris.com>
2024-11-09 19:30:03 +0000

    feat(tidy up auto completion)

--- conf/new-nvim-setup/lua/core/plugin_config/completions.lua
+++ conf/new-nvim-setup/lua/core/plugin_config/completions.lua
@@ -1,27 +1,77 @@
 local cmp = require("cmp")
-
 require("luasnip.loaders.from_vscode").lazy_load()
 
 cmp.setup({
   mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-o>'] = cmp.mapping.complete(),
+    -- Documentation scrolling
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+
+    -- Cancel completion
     ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+
+    -- Manual trigger completion
+    ['<C-Space>'] = cmp.mapping.complete(),
+
+    -- Navigate completion menu
     ['<C-j>'] = cmp.mapping.select_next_item(),
     ['<C-k>'] = cmp.mapping.select_prev_item(),
+
+    -- Tab completion as an alternative to Ctrl-j/k
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+
+    -- Confirm completion
+    ['<CR>'] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    }),
   }),
+
+  -- Completion menu appearance
+  window = {
+    documentation = {
+      max_height = 15,
+      max_width = 60,
+      border = 'rounded',
+      winhighlight = 'NormalFloat:Normal,FloatBorder:Normal',
+    },
+    completion = {
+      border = 'rounded',
+      winhighlight = 'NormalFloat:Normal,FloatBorder:Normal',
+    },
+  },
+
+  -- Snippet support
   snippet = {
     expand = function(args)
       require('luasnip').lsp_expand(args.body)
     end,
   },
-  sources = cmp.config.sources({
-    { name = 'supermaven' },
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-  }, {
-    { name = 'buffer' },
-  }),
+
+  -- Completion sources in priority order
+  sources = {
+    { name = 'supermaven', priority = 1000 }, -- AI completions
+    { name = 'nvim_lsp',   priority = 900 },  -- LSP
+    { name = 'luasnip',    priority = 800 },  -- Snippets
+    { name = 'buffer',     priority = 500 },  -- Buffer words
+  },
+
+  -- Completion behavior
+  completion = {
+    completeopt = 'menu,menuone,noinsert',
+  },
 })
