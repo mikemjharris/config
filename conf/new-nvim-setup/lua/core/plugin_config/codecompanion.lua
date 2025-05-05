6005c5f0d58ae859b6d9bf362a03b59412b57bdd
Mike Harris <hello@mikemjharris.com>
2025-05-05 21:07:38 +0100

    fix(use gpg key )

--- conf/new-nvim-setup/lua/core/plugin_config/codecompanion.lua
+++ conf/new-nvim-setup/lua/core/plugin_config/codecompanion.lua
@@ -1,4 +1,13 @@
 require("codecompanion").setup({
+  adapters = {
+    openai = function()
+      return require("codecompanion.adapters").extend("openai", {
+        env = {
+          api_key = "cmd:gpg --quiet --batch --decrypt ~/.secrets/openai_api_key.gpg",
+        },
+      })
+    end,
+  },
   strategies = {
     chat = {
       adapter = "openai",
