568240dd6dc8e8f3b5c37876871b92a9b5458ffb
Mike Harris <hello@mikemjharris.com>
2025-07-22 13:44:24 +0100

    feat(update avnate setup)

--- conf/new-nvim-setup/lua/core/plugins.lua
+++ conf/new-nvim-setup/lua/core/plugins.lua
@@ -173,13 +173,17 @@ local plugins = {
       -- add any opts here
       -- for example
       provider = "openai",
-      openai = {
-        endpoint = "https://api.openai.com/v1",
-        model = "gpt-4o",             -- your desired model (or use gpt-4o, etc.)
-        timeout = 30000,              -- Timeout in milliseconds, increase this for reasoning models
-        temperature = 0,
-        max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-        --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+      providers = {
+        openai = {
+          endpoint = "https://api.openai.com/v1",
+          model = "gpt-4o",             -- your desired model (or use gpt-4o, etc.)
+          timeout = 30000,              -- Timeout in milliseconds, increase this for reasoning models
+          extra_req_params = {
+            temperature = 0,
+            max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+            --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+          }
+        },
       },
     },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
