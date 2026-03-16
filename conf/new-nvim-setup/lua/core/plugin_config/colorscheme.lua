95774f072cb5cf715edbe89bcebfa73bde0a1ba8
Mike <hello@mikemjharris.com>
2026-03-16 19:27:56 +0000

    feat: colors for csv

--- conf/new-nvim-setup/lua/core/plugin_config/colorscheme.lua
+++ conf/new-nvim-setup/lua/core/plugin_config/colorscheme.lua
@@ -6,3 +6,19 @@ vim.cmd [[ colorscheme PaperColorSlimLight ]]
 vim.api.nvim_set_hl(0, 'CursorLine', { bg = '#d0eaff' })
 
 vim.opt.signcolumn = 'yes'
+
+-- csvview.nvim column colors (light theme friendly)
+local csv_colors = {
+  '#2e7d32', -- green
+  '#1565c0', -- blue
+  '#6a1b9a', -- purple
+  '#e65100', -- orange
+  '#00838f', -- teal
+  '#ad1457', -- pink
+  '#4e342e', -- brown
+  '#283593', -- indigo
+  '#558b2f', -- lime
+}
+for i, color in ipairs(csv_colors) do
+  vim.api.nvim_set_hl(0, 'CsvViewCol' .. (i - 1), { fg = color })
+end
